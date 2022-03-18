0b0e8182a222ace8609721277d8706d586dc44f0
Ming119 <ming2007119@gmail.com>
2022-03-19 02:00:15 +0800

    HW1 2.15

---
+++ HW1/Chap2.15/src/main.c
@@ -0,0 +1,95 @@
+/*
+ *  Chap2.15/src/main.c
+ *
+ *  108590050
+ *  Programming Problem Chap. 2.15
+ * 
+ */  
+
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#ifndef BUF_SIZE
+#define BUF_SIZE     64
+#endif
+
+void usage(char *programName)
+{
+    printf("Usage:\n\t%s <src> <dest>\n", programName);
+}
+
+void fcopy(char *f1, char *f2)
+{   
+    // Open the input file
+    int inFd;
+    if ((inFd = open(f1, O_RDONLY)) == -1)  // If file doesn't exist
+    {   
+        // Abort
+        perror("Error opening source file");    
+        exit(EXIT_FAILURE);
+    }
+
+    // Create output file
+    int outFd;
+    if ((outFd = open(f2, O_EXCL | O_CREAT | O_WRONLY, S_IRWXU | S_IRWXG | S_IRWXO)) == -1) // If file exists
+    {   
+        // Abort
+        perror("Error");
+        close(inFd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Loop
+    ssize_t n;
+    char buffer[BUF_SIZE];
+    while ((n = read(inFd, buffer, BUF_SIZE)) > 0)  // Read from input file, until read fails
+    {
+        if (write(outFd, buffer, n) != n)           // Write to output file
+        {
+            perror("Error copying file");
+            close(inFd);
+            close(outFd);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    
+    close(inFd);    // Close input file
+    close(outFd);   // Close output file
+
+    printf("Copy complete!");   // Write completion message to screen
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 2 && !(strcmp(argv[1], "-h") && strcmp(argv[1], "--help")))
+    {
+        usage(argv[0]);
+        exit(EXIT_SUCCESS);
+    }
+    
+    if (argc == 3)
+    {
+        fcopy(argv[1], argv[2]);
+
+    } else
+    {
+        char f1[BUF_SIZE], f2[BUF_SIZE];
+
+        // Acquire input file name
+        printf("Input a source file: ");    // Write prompt to screen
+        scanf("%[^\n]%*c", f1);             // Accept input
+
+        // Acquire output file name
+        printf("Input a destination file: ");   // Write prompt to screen
+        scanf("%[^\n]%*c", f2);                 // Accept input
+
+        fcopy(f1, f2);
+    }
+
+    exit(EXIT_SUCCESS); // Terminate normally
+}
